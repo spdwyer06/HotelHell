41be6fcc7ebdb53422ba1e0b121aafc70c0732fd
Sean Dwyer <spdwyer06@gmail.com>
2021-05-09 12:43:12 -0400

    Add Room models

---
+++ HotelHell_Models/Room/RoomCreate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelHell_Models.Room
+{
+    public class RoomCreate
+    {
+        [Required]
+        public int HotelId { get; set; }
+
+        [Required]
+        [Display(Name = "Room Number")]
+        public int RoomNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Number Of Beds")]
+        public int NumOfBeds { get; set; }
+    }
+}
---
+++ HotelHell_Models/Room/RoomListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelHell_Models.Room
+{
+    public class RoomListItem
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Hotel Name")]
+        public string HotelName { get; set; }
+
+        [Display(Name = "Number Of Beds")]
+        public int NumOfBeds { get; set; }
+    }
+}
