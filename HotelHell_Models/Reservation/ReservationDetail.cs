12c0a5e87d7524405101d412352347a38706f6a0
Sean Dwyer <spdwyer06@gmail.com>
2021-05-12 14:51:51 -0400

    Add Reservation controller

--- HotelHell_Models/Reservation/ReservationDetail.cs
+++ HotelHell_Models/Reservation/ReservationDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace HotelHell_Models.Reservation
 {
     public class ReservationDetail : ReservationListItem
     {
+        [Display(Name = "Customer Id")]
+        public int CustomerId { get; set; }
+
+        [Display(Name = "Room Id")]
+        public int RoomId { get; set; }
     }
 }
